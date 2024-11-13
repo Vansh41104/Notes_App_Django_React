9239516a67fdb34d7156dcf2393fd86ae045bb7f
Vansh41104 <vanshbhatnagar445@gmail.com>
2024-11-13 23:47:26 +0530

    Add Docker support and update project dependencies
    - Introduced Dockerfiles for backend and frontend services
    - Added docker-compose.yml for managing multi-container setup
    - Updated backend settings to use PostgreSQL instead of SQLite
    - Added package.json for frontend dependencies
    - Updated .gitignore to exclude additional files
    - Changed title in index.html to "Notes App"

--- backend/notes/notes/settings.py
+++ backend/notes/notes/settings.py
@@ -12,10 +12,10 @@ https://docs.djangoproject.com/en/5.0/ref/settings/
 
 from pathlib import Path
 from datetime import timedelta
-from dotenv import load_dotenv
+# from dotenv import load_dotenv
 import os
 
-load_dotenv()
+# load_dotenv()
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -98,12 +98,14 @@ WSGI_APPLICATION = 'notes.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django',
+        'USER': 'postgres',
+        'PASSWORD': '6104',
+        'HOST': 'host.docker.internal',
+        'PORT': '5432',
     }
 }
-
-
 # Password validation
 # https://docs.djangoproject.com/en/5.0/ref/settings/#auth-password-validators
 
