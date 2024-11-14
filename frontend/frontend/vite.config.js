5301bbdb5dcc24e7db706b830c83ccf75c59c434
Vansh41104 <vanshbhatnagar445@gmail.com>
2024-11-14 22:42:35 +0530

    Remove package.json and package-lock.json; add Nginx configuration and update Dockerfile for production build

--- frontend/frontend/vite.config.js
+++ frontend/frontend/vite.config.js
@@ -4,4 +4,8 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5174,
+    host: '0.0.0.0', // to expose to other containers
+  }
 })
